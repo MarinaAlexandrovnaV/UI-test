97136c7b550b405d20c1414a96fc281e340fbcfe
[Marina] <[marisha071997@gmail.com]>
2023-04-25 11:17:44 +0300

    check button text on google home page

--- pages/index_page.py
+++ pages/index_page.py
@@ -3,6 +3,14 @@ import config
 
 
 class IndexPage:
+    _LINK_ENG_LANG = "xpath=//*[@id='SIvCob']/a"
+    _BUTTON_GOOGLE_SEARCH = "xpath=/html/body/div[1]/div[3]/form/div[1]/div[1]/div[4]/center/input[1]"
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def press_link_eng_lang(self, page: Page):
+        page.locator(self._LINK_ENG_LANG).click()
+
+    def get_text_from_google_search_button(self, page: Page):
+        return page.locator(self._BUTTON_GOOGLE_SEARCH).get_attribute('value')
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -6,4 +6,8 @@ class TestFooter:
 
     def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
         pages.index_page.open_index_page(page)
-        time.sleep(10)
+        pages.index_page.press_link_eng_lang(page)
+        actual_result = pages.index_page.get_text_from_google_search_button(page)
+        assert actual_result == "Google Search", "'Google Search button text is not correct"
+
+
